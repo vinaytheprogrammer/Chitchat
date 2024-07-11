5e57bdb821310eba22409211363b3465c0c090d6
Rithik Yadav <cow17boy@gmail.com>
2024-07-11 11:44:33 +0530

    Reverted chanegs

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
 const { GoogleGenerativeAI } = require("@google/generative-ai");
@@ -10,27 +10,24 @@ app.use(bodyParser.json());
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
+app.use(express.static("public"));
 
-app.use(express.static('public'));
+app.post("/chat", async (req, res) => {
+  async function run() {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-app.post('/chat', async (req, res) => {
-async function run() {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    const prompt = req.body.message;
 
-  const prompt = req.body.message;
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    //   console.log(text);
+    res.json({ message: text });
+  }
 
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-//   console.log(text);
-  res.json({ message: text });
-}
-
-run();
-
+  run();
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
