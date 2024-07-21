d6592403bf8d515c86d3c0971c489e8950c6fe04
HemantChaudhary11 <hemant.chaudhary8171@gmail.com>
2024-07-21 23:13:55 +0530

    added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require("dotenv");
 dotenv.config();
